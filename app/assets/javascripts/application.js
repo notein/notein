9362e1b02e51bb48aab837fe9e212849568bc90e
Jan Wiegelmann <git@wiegelmann.net>
2018-03-22 18:29:03 +0100

    trix styling in editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require trix
 //= require rails-ujs
 //= require_tree .
@@ -80,7 +68,6 @@ addEventListener("trix-attachment-add", function(event) {
 // open all external links in a new window
 addEventListener("click", function(event) {
   var el = event.target
-  console.log(el.tagName)
   if (el.tagName === "A" && !el.isContentEditable && el.host !== window.location.host) {
     el.setAttribute("target", "_blank")
   }
